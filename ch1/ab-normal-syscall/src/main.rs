578e7a90c2a143a7a5a7b6cd56070d766f7b0fc6
Carl Fredrik Samson <cf@samson.no>
2023-05-12 14:04:10 +0200

    added normal syscall example

---
+++ ch1/ab-normal-syscall/src/main.rs
@@ -0,0 +1,74 @@
+use std::io;
+
+fn main() {
+    let message = "Hello world from syscall!\n";
+    let message = String::from(message);
+    syscall(message).unwrap();
+}
+
+// ----------------------------------------------------------------------------
+// Normal syscall on Linux/macOS
+// ----------------------------------------------------------------------------
+
+#[cfg(target_family = "unix")]
+#[link(name = "c")]
+extern "C" {
+    fn write(fd: u32, buf: *const u8, count: usize) -> i32;
+}
+
+#[cfg(target_family = "unix")]
+fn syscall(message: String) -> io::Result<()> {
+    let msg_ptr = message.as_ptr();
+    let len = message.len();
+    let res = unsafe { write(1, msg_ptr, len) };
+
+    if res == -1 {
+        return Err(io::Error::last_os_error());
+    }
+
+    Ok(())
+}
+
+// ----------------------------------------------------------------------------
+// Normal syscall on Windows
+// ----------------------------------------------------------------------------
+
+#[cfg(target_family = "windows")]
+#[link(name = "kernel32")]
+extern "stdcall" {
+    /// https://docs.microsoft.com/en-us/windows/console/getstdhandle
+    fn GetStdHandle(nStdHandle: i32) -> i32;
+    /// https://docs.microsoft.com/en-us/windows/console/writeconsole
+    fn WriteConsoleW(
+        hConsoleOutput: i32,
+        lpBuffer: *const u16,
+        numberOfCharsToWrite: u32,
+        lpNumberOfCharsWritten: *mut u32,
+        lpReserved: *const std::ffi::c_void,
+    ) -> i32;
+}
+
+#[cfg(target_os = "windows")]
+fn syscall(message: String) -> io::Result<()> {
+
+    // let's convert our utf-8 to a format windows understands
+    let msg: Vec<u16> = message.encode_utf16().collect();
+    let msg_ptr = msg.as_ptr();
+    let len = msg.len() as u32;
+
+    let mut output: u32 = 0;
+        let handle = unsafe { GetStdHandle(-11) };
+        if handle  == -1 {
+            return Err(io::Error::last_os_error())
+        }
+
+        let res = unsafe {
+            WriteConsoleW(handle, msg_ptr, len, &mut output, std::ptr::null())
+            };
+        if res  == 0 {
+            return Err(io::Error::last_os_error());
+        }
+
+    assert_eq!(output, len);
+    Ok(())
+}
