e3510b82b4f86e5d6a1e695f0007fd87d1716737
Carl Fredrik Samson <cf@samson.no>
2023-10-05 01:12:50 +0200

    several fixes

--- ch4/a-epoll/src/main.rs
+++ ch4/a-epoll/src/main.rs
@@ -1,4 +1,8 @@
-use std::{io::{self, Result}, thread::sleep, time::Duration};
+use std::{
+    io::{self, Result},
+    thread::sleep,
+    time::Duration,
+};
 
 use net::TcpStream;
 use poll::Poll;
@@ -7,13 +11,12 @@ mod ffi;
 mod net;
 mod poll;
 
-
 fn send_requests(poll: &Poll) -> Result<Vec<TcpStream>> {
     let mut streams = vec![];
     let addr = "localhost:8080";
 
     for i in 0..5 {
-        let delay = (5-i) * 1000;
+        let delay = (5 - i) * 1000;
         let request = format!(
             "GET /{}/request-{} HTTP/1.1\r\n\
              Host: localhost\r\n\
@@ -30,18 +33,59 @@ fn send_requests(poll: &Poll) -> Result<Vec<TcpStream>> {
     Ok(streams)
 }
 
+/// This is the example from the book (in the book we put the code in `main`
+/// instead of this function). The function waits for events using epoll and
+/// handles the events as they arrive.
+fn wait_with_epoll() -> Result<()> {
+    let mut poll = poll::Poll::new()?;
+    let streams = send_requests(&poll)?;
+    let mut events_to_handle = streams.len();
+
+    while events_to_handle > 0 {
+        let mut events = Vec::with_capacity(10);
+        poll.poll(&mut events, None)?;
+
+        for event in events {
+            let index = event.epoll_data;
+            let mut data = vec![0u8; 4096];
+            match streams[index].read(&mut data) {
+                Ok(n) => {
+                    let txt = String::from_utf8_lossy(&data[..n]);
+                    println!("RECEIVED: {:?}", event);
+                    println!("{txt}\n------\n");
+
+                    // we consider one event as handled
+                    events_to_handle -= 1;
+                    break;
+                }
+
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    println!("FINISHED");
+    Ok(())
+}
+
+/// This function is just an example on how you can experiment with this code.
+/// Here we send the requests and immidiately try to read data from socket #4.
+/// Data won't be ready yet so we get a WouldBlock error until the data is
+/// ready to be read on the socket.
+#[allow(dead_code)]
 fn poll_in_loop() -> Result<()> {
     let poll = poll::Poll::new()?;
     let streams = send_requests(&poll)?;
 
     loop {
-        let mut data = vec![0u8; 4098];
+        let mut data = vec![0u8; 4096];
         // poll the last one in stream (delay = 1000 ms)
         match streams[4].read(&mut data) {
             Ok(n) if n == 0 => {
                 println!("Got 0 bytes. No more data!");
                 break;
-            },
+            }
             Ok(n) => {
                 let txt = String::from_utf8_lossy(&data[..n]);
                 println!("RESPONSE STREAM 4:\n{txt}");
@@ -57,35 +101,77 @@ fn poll_in_loop() -> Result<()> {
     Ok(())
 }
 
-fn with_epoll_wait() -> Result<()> {
+/// This is the example from the book (in the book we put the code in `main`
+/// instead of this function). The function waits for events using epoll and
+/// handles the events as they arrive.
+#[allow(dead_code)]
+fn wait_with_epoll_extended() -> Result<()> {
     let mut poll = poll::Poll::new()?;
     let streams = send_requests(&poll)?;
     let mut events_to_handle = streams.len();
 
     while events_to_handle > 0 {
         let mut events = Vec::with_capacity(10);
-        poll.poll(&mut events, None)?;
+
+        // experiment setting different timeouts
+        poll.poll(&mut events, Some(100))?;
+
+        if events.is_empty() {
+            // We either got a timeout or a spurious wakeup.
+            println!("TIMEOUT");
+            continue;
+        }
 
         for event in events {
             let index = event.epoll_data;
-            let mut data = vec![0u8; 4098];
-            
+            let mut data = vec![0u8; 4096];
+
+            // We should always read in a loop since we can get
+            // more data than our buffer can hold
             loop {
                 match streams[index].read(&mut data) {
-                Ok(n) if n == 0 => break,
-                Ok(n) => {
-                    let txt = String::from_utf8_lossy(&data[..n]);
-                    println!("RECEIVED: {:?}", event);
-                    println!("{txt}\n------\n");
-                    // we consider one event as handled
-                    events_to_handle -= 1;
-                }
+                    /*
+                    The only way we should get a count of 0 is if the
+                    size of the data recieved was just large enough to fill
+                    the entire buffer so we had to call read again to check
+                    if there was more data on the socket. If that happens,
+                    we conclude that the event is handled and stop reading
+                    the socket
+                    */
+                    Ok(n) if n == 0 => {
+                        events_to_handle -= 1;
+                        break;
+                    }
+                    Ok(n) => {
+                        let txt = String::from_utf8_lossy(&data[..n]);
+                        println!("RECEIVED: {:?}", event);
+                        println!("{txt}\n------\n");
 
-                Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
-                Err(e) => return Err(e),
+                        /*
+                        If we get less data than our buffer could hold we
+                        consider the event as handled.
+
+                        In real life this
+                        might be a bit more complicated since you can't
+                        always rely on all the data to come in one read call.
+                        If you expect more data related to handling the event
+                        you would need to re-enable the event in the epoll
+                        instance using `epoll_ctl` with the `EPOLL_CTL_MOD`
+                        argument, since epoll was configured with
+                        EPOLLONESHOT for this event. This way you will get
+                        further notifications when more data is available.
+                        */
+                        if n < data.len() {
+                            events_to_handle -= 1;
+                            break;
+                        }
+                    }
+                    // This error could occur on both writes and reads under
+                    // certain conditions and should be expected.
+                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                    Err(e) => return Err(e),
+                }
             }
-            }
-            
         }
     }
 
@@ -94,6 +180,8 @@ fn with_epoll_wait() -> Result<()> {
 }
 
 fn main() -> Result<()> {
+    wait_with_epoll()
+    // wait_with_epoll_extended()
     //poll_in_loop()
-    with_epoll_wait()
+
 }
--- ch4/a-epoll/src/poll.rs
+++ ch4/a-epoll/src/poll.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Result};
 
-use crate::{ffi, net::{self, TcpStream}};
+use crate::{ffi, net::TcpStream};
 
 type Events = Vec<ffi::Event>;
 
@@ -23,15 +23,14 @@ impl Poll {
     pub fn registry(&self) -> &Registry {
         &self.registry
     }
-    
+
     /// Makes a blocking call to the OS parking the calling thread. It will wake up
     /// when one or more events we've registered interest in have occurred or
     /// the timeout duration has elapsed, whichever occurs first.
-    /// 
-    /// # Errors
-    /// If an error of the type `io::ErrorKind::Interrupted` is reported it
-    /// means that the call timed out. This error condition should be expected
-    /// and handled by the caller of this function.
+    ///
+    /// # Note
+    /// If the number of events returned is 0, the wakeup was due to an elapsed
+    /// timeout
     pub fn poll(&mut self, events: &mut Events, timeout: Option<i32>) -> Result<()> {
         let fd = self.registry.raw_fd;
         let timeout = timeout.unwrap_or(-1);
@@ -54,7 +53,7 @@ pub struct Registry {
 }
 
 impl Registry {
-    
+
     // NB! Mio inverts this, and `source` owns the register implementation
     pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<()> {
         let mut event = ffi::Event {
@@ -66,7 +65,7 @@ impl Registry {
         let res = unsafe {
             ffi::epoll_ctl(self.raw_fd, op, source.as_raw_fd(), &mut event)
         };
-        
+
         if res < 0 {
             return Err(io::Error::last_os_error());
         }
@@ -77,7 +76,7 @@ impl Registry {
 impl Drop for Registry {
     fn drop(&mut self) {
         let res = unsafe { ffi::close(self.raw_fd) };
-        
+
         if res < 0 {
             // Note! Mio logs the error but does not panic!
             let err = io::Error::last_os_error();
