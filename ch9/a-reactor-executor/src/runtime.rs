0454b2c3e43c705462a6daeaf3be7915a1d82dde
Carl Fredrik Samson <cf@samson.no>
2023-11-09 00:21:23 +0100

    working on ch 9

--- ch9/a-reactor-executor/src/runtime.rs
+++ ch9/a-reactor-executor/src/runtime.rs
@@ -1,102 +1,150 @@
-use std::{collections::HashMap, sync::{Arc, Mutex, atomic::{AtomicUsize, Ordering}}};
-
-use mio::{net::TcpStream, Token, Interest, Events};
-
-use crate::future::{Future, PollState};
+use crate::future::Future;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex, OnceLock};
+use std::cell::RefCell;
 
 thread_local! {
-    static E: Executor = Executor::new();
+    static TASKS: RefCell<VecDeque<Arc<dyn Future<Output = String>>>> = RefCell::new(VecDeque::new());
+    static CURR_ID: RefCell<usize> = RefCell::new(0);
 }
 
-static EXECUTOR: ExecWrapper = ExecWrapper;
+fn next_id() -> usize {
+    CURR_ID.with(|id| {
+        let mut id = id.borrow_mut();
+        *id += 1;
+        *id
+    })
+}
 
-struct ExecWrapper;
+pub struct Task {
+    future: Arc<dyn Future<Output = String>>,
+    id: usize,
+}
 
-impl ExecWrapper {
-    pub fn set_ready(&self, id: usize) {
-        E.with(|executor| executor.set_ready(id))
+impl Task {
+    pub fn new<F>(future: F) -> Self
+    where
+        F: Future<Output = String> + 'static,
+    {
+        Self {
+            future: Arc::new(future),
+            id: next_id(),
+        }
     }
 }
 
-pub struct Executor {
-    ready: Arc<Mutex<Vec<Box<dyn Future<Output = String>>>>>,
-    pending: Arc<Mutex<HashMap<usize, Box<dyn Future<Output = String>>>>>,
-    task_counter: AtomicUsize,
-}
+pub struct Executor;
 
 impl Executor {
     pub fn new() -> Self {
-        Self {
-            ready: Arc::new(Mutex::new(vec![])),
-            pending: Arc::new(Mutex::new(HashMap::new())),
-            task_counter: AtomicUsize::new(0),
-        }
+        Self {}
     }
 
-    pub fn spawn<F: Future<Output = String> + 'static>(&self, task: F) {
-        self.ready.lock().map(|mut r| r.push(Box::new(task))).unwrap();
-        self.task_counter.fetch_add(1, Ordering::Relaxed);
-    }
+    pub fn spawn(&mut self, f: impl Future<Output = String> + 'static) {}
 
-    pub fn run(&self) {
-        while self.task_counter.load(Ordering::Relaxed) > 0 {
-            while let Some(mut task) = self.ready.lock().map(|mut r| r.pop()).unwrap() {
-                match task.poll() {
-                    PollState::NotReady => {
-                        // task is responsible for wakeup
-                        self.pending.lock().map(|mut p| p.insert(k, v))
-                        println!("NotReady");
-
-                    }
-
-                    PollState::Ready(_) => {
-                        self.task_counter.fetch_sub(1, Ordering::Relaxed);
-                    }
-                }
-            }
-        }
-
-        // sleep on reactor.poll()
-    }
-
-    pub fn set_ready(&self, id: usize) {
-        let task = self.pending.lock().map(|mut p| p.remove(&id).unwrap()).unwrap();
-        self.ready.lock().map(|mut r| r.push(task)).unwrap();
+    pub fn block_on(&self) {
+        
     }
 }
 
+// use std::{collections::HashMap, sync::{Arc, Mutex, atomic::{AtomicUsize, Ordering}}};
 
-pub struct Reactor {
-    poll: mio::Poll,
-    next_id: usize,
-}
+// use mio::{net::TcpStream, Token, Interest, Events};
 
-impl Reactor {
-    pub fn new() -> Self {
-        Self {
-            poll: mio::Poll::new().unwrap(),
-            next_id: 1,
-        }
-    }
+// use crate::future::{Future, PollState};
 
-    pub fn poll(&mut self) {
-        let mut events = Events::with_capacity(10);
-        self.poll.poll(&mut events, None).unwrap();
+// thread_local! {
+//     static E: Executor = Executor::new();
+// }
 
-        for event in &events {
-            let Token(id) = event.token();
-            EXECUTOR.set_ready(id);
-        }
-    }
+// static EXECUTOR: ExecWrapper = ExecWrapper;
 
-    pub fn register(&mut self, source: &mut TcpStream) -> usize {
-        let id = self.next_id;
-        self.poll.registry().register(source, Token(id), Interest::READABLE).unwrap();
-        self.next_id += 1;
-        id
-    }
+// struct ExecWrapper;
 
-    pub fn registry(&self) -> mio::Registry {
-        self.poll.registry().try_clone().unwrap()
-    }
-}
+// impl ExecWrapper {
+//     pub fn set_ready(&self, id: usize) {
+//         E.with(|executor| executor.set_ready(id))
+//     }
+// }
+
+// pub struct Executor {
+//     ready: Arc<Mutex<Vec<Box<dyn Future<Output = String>>>>>,
+//     pending: Arc<Mutex<HashMap<usize, Box<dyn Future<Output = String>>>>>,
+//     task_counter: AtomicUsize,
+// }
+
+// impl Executor {
+//     pub fn new() -> Self {
+//         Self {
+//             ready: Arc::new(Mutex::new(vec![])),
+//             pending: Arc::new(Mutex::new(HashMap::new())),
+//             task_counter: AtomicUsize::new(0),
+//         }
+//     }
+
+//     pub fn spawn<F: Future<Output = String> + 'static>(&self, task: F) {
+//         self.ready.lock().map(|mut r| r.push(Box::new(task))).unwrap();
+//         self.task_counter.fetch_add(1, Ordering::Relaxed);
+//     }
+
+//     pub fn run(&self) {
+//         while self.task_counter.load(Ordering::Relaxed) > 0 {
+//             while let Some(mut task) = self.ready.lock().map(|mut r| r.pop()).unwrap() {
+//                 match task.poll() {
+//                     PollState::NotReady => {
+//                         // task is responsible for wakeup
+//                         self.pending.lock().map(|mut p| p.insert(k, v))
+//                         println!("NotReady");
+
+//                     }
+
+//                     PollState::Ready(_) => {
+//                         self.task_counter.fetch_sub(1, Ordering::Relaxed);
+//                     }
+//                 }
+//             }
+//         }
+
+//         // sleep on reactor.poll()
+//     }
+
+//     pub fn set_ready(&self, id: usize) {
+//         let task = self.pending.lock().map(|mut p| p.remove(&id).unwrap()).unwrap();
+//         self.ready.lock().map(|mut r| r.push(task)).unwrap();
+//     }
+// }
+
+// pub struct Reactor {
+//     poll: mio::Poll,
+//     next_id: usize,
+// }
+
+// impl Reactor {
+//     pub fn new() -> Self {
+//         Self {
+//             poll: mio::Poll::new().unwrap(),
+//             next_id: 1,
+//         }
+//     }
+
+//     pub fn poll(&mut self) {
+//         let mut events = Events::with_capacity(10);
+//         self.poll.poll(&mut events, None).unwrap();
+
+//         for event in &events {
+//             let Token(id) = event.token();
+//             EXECUTOR.set_ready(id);
+//         }
+//     }
+
+//     pub fn register(&mut self, source: &mut TcpStream) -> usize {
+//         let id = self.next_id;
+//         self.poll.registry().register(source, Token(id), Interest::READABLE).unwrap();
+//         self.next_id += 1;
+//         id
+//     }
+
+//     pub fn registry(&self) -> mio::Registry {
+//         self.poll.registry().try_clone().unwrap()
+//     }
+// }
